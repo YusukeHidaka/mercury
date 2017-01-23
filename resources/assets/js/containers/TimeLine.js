796daba11c796a5d6847669885fda1a2935f3fa0
YusukeHidaka <ysk91.akg@gmail.com>
2017-01-24 00:04:49 +0900

    call plans api

--- resources/assets/js/containers/TimeLine.js
+++ resources/assets/js/containers/TimeLine.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react'
+import request from 'axios';
 import {Link} from 'react-router';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import {CommonConstants} from '../constants';
+import {PostModal, PlanList} from '../components';
 
-export default class TimeLine extends React.Component{
+class TimeLine extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowPostModal: false,
+      plans: []
+    }
+  }
+
+  componentWillMount() {
+    request.get(CommonConstants.API_GET_PLANS_PATH).then((response) => {
+      this.setState({plans: response.data})
+      console.log(response.data);
+    }).catch((response) => {
+      console.log(response)
+    })
+  }
+
+  openPostModal() {
+    this.setState({isShowPostModal: true});
+  }
+
+  closePostModal() {
+    this.setState({isShowPostModal: false});
+  }
+
+  postPlanData() {
+    //TODO post data to google api
+    this.closePostModal();
+  }
+
   render(){
     return (
       <div className='time-line'>
+        <div>
+          <h2>プラン一覧</h2>
+          <PlanList plans={this.state.plans}/>
+        </div>
         <nav>
           <Link to="#">条件から検索</Link>
           <Link to="/plans/:planId">プラン詳細</Link>
@@ -14,10 +51,15 @@ export default class TimeLine extends React.Component{
         </nav>
         {this.props.children}
 
-        <div className='post-button btn'>
+        <div className='post-button btn' onClick={this.openPostModal.bind(this)}>
           <div className='post-sentence'>投稿</div>
           <i className="fa fa-pencil-square-o post-icon" aria-hidden="true"></i>
         </div>
+        <PostModal
+          isShow={this.state.isShowPostModal}
+          okCallback={this.postPlanData.bind(this)}
+          cancelCallback={this.closePostModal.bind(this)}
+        />
       </div>
     );
   }
